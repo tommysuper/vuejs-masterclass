7cdb4c2bbe5d51a03726b8a8cc7a3d4892aabce2
tommy <hekailong@hotmail.com>
2024-10-03 00:02:53 -0400

    Oct2

--- database/seed.js
+++ database/seed.js
@@ -4,8 +4,21 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.VITE_SUPERBASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY)
 
+const logErrorAndExit = (tableName, error) => {
+  console.error(
+    `An error occurred in table '${tableName}' with code ${error.code}: ${error.message}`
+  )
+  process.exit(1)
+}
+
+const logStep = (stepMessage) => {
+  console.log(stepMessage)
+}
+
 const seedProjects = async (numEntries) => {
+  logStep('Seeding projects...')
   const projects = []
+
   for (let i = 0; i < numEntries; i++) {
     const name = faker.lorem.words(3)
 
@@ -13,10 +26,48 @@ const seedProjects = async (numEntries) => {
       name: name,
       slug: name.toLocaleLowerCase().replace(/ /g, '-'),
       status: faker.helpers.arrayElement(['in-progress', 'completed']),
-      collaborators: faker.helpers.arrayElements(['1,2,3'])
+      collaborators: faker.helpers.arrayElements([1, 2, 3])
     })
   }
-  const { error } = await supabase.from('projects').insert(projects)
+
+  const { data, error } = await supabase.from('projects').insert(projects).select('id')
+
+  if (error) return logErrorAndExit('Projects', error)
+
+  logStep('Projects seeded successfully.')
+
+  return data
 }
 
-await seedProjects(10)
+const seedTasks = async (numEntries, projectsIds) => {
+  logStep('Seeding tasks...')
+  const tasks = []
+
+  for (let i = 0; i < numEntries; i++) {
+    tasks.push({
+      name: faker.lorem.words(3),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      description: faker.lorem.paragraph(),
+      due_date: faker.date.future(),
+      project_id: faker.helpers.arrayElement(projectsIds),
+      collaborators: faker.helpers.arrayElements([1, 2, 3])
+    })
+  }
+
+  const { data, error } = await supabase.from('tasks').insert(tasks).select('id')
+
+  if (error) return logErrorAndExit('Tasks', error)
+
+  logStep('Tasks seeded successfully.')
+
+  return data
+}
+
+const seedDatabase = async (numEntriesPerTable) => {
+  const projectsIds = (await seedProjects(numEntriesPerTable)).map((project) => project.id)
+  await seedTasks(numEntriesPerTable, projectsIds)
+}
+
+const numEntriesPerTable = 10
+
+seedDatabase(numEntriesPerTable)
--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -2,22 +2,21 @@
 import { supabase } from '@/lib/supabaseClient'
 import { ref } from 'vue'
 import type { Tables } from 'database/types'
-const projects = ref<Tables<'projects'>[] | null>(null)
+const tasks = ref<Tables<'tasks'>[] | null>(null)
 ;(async () => {
-  const { data, error } = await supabase.from('projects').select()
+  const { data, error } = await supabase.from('tasks').select()
   if (error) console.log(error)
 
-  projects.value = data
-  console.log('Porjects: ', projects.value)
+  tasks.value = data
 })()
 </script>
 <template>
   <div>
-    <h1>Projects Page</h1>
+    <h1>Tasks Page</h1>
     <router-link to="/"> Go to Home</router-link>
     <ul>
-      <li v-for="project in projects" :key="project.id">
-        {{ project.name }}
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.name }}
       </li>
     </ul>
   </div>
